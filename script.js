8377dc42893603cb10d1e8b38924ba47c9ab570d
LLIHUP <LLIHUP@gmail.com>
2023-01-15 19:41:29 +0200

    3

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
 
 'use strict';
 //1
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '10');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '10');
 
 //2
 const personalMovieDB = {
@@ -34,3 +34,15 @@ const personalMovieDB = {
     privat: false
 
 };
+
+//3
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
